3aaedb421d61b3ba49d7eec7cdecafacb6baff11
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-08-11 12:17:18 -0700

    Database settings

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "muowdopceqgxjn2b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "velldzqwlvhfvewx",
-  password: "trrptgchg2yigwnt",
-  database: "huywl9aqmwttuk8t"
-});
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection({
+    host: "muowdopceqgxjn2b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "velldzqwlvhfvewx",
+    password: "trrptgchg2yigwnt",
+    database: "huywl9aqmwttuk8t"
+  });  
+}else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  })
+}
 
 // Make connection.
 connection.connect(function(err) {
